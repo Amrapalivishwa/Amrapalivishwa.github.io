dc23ae0d6fe3957c692e346d0ec964ff7ac71aba
Amrapalivishwa <amrapali@servelots.com>
2016-11-30 18:37:24 +0530

    Western India folk dance story

---
+++ folk-dnaces-of-western-india/Maharashtra_folk_dances /js/chapters.js
@@ -0,0 +1,14 @@
+window.chapters = [
+                   {el: "#story-content",
+                    "who":"scribe",
+                    "what":"img-anno",
+                    "tag": "lsd-art"},
+		   {el: "#story-content",
+                    "who":"Amrapali",
+                    "what":"img-anno",
+                    "tag": "gujarat"},
+                   {el: "#story-content",
+                    "who":"Amrapali",
+                    "what":"txt-anno",
+                    "tag": "gujarat"},
+                    ];
